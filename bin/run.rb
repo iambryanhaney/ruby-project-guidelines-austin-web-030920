94e9096e9175859f2730da06042b12925afd96bb
agomezdemen <agomezdemen15@gmail.com>
2020-03-25 15:28:56 -0500

    user input :)

--- bin/run.rb
+++ bin/run.rb
@@ -5,4 +5,8 @@ require_relative '../config/environment'
 uri = URI.parse("https://api.seatgeek.com/2/events?client_id=MjExNDI3ODR8MTU4NTE2MzM2OS43")
 response = Net::HTTP.get_response(uri)
 random_var_name = JSON.parse(response.body)
-binding.pry
+
+(system "clear") || system("cls")
+puts "Welcome to project_name\n1. New User\n2. Login\n\n>"
+response = gets.chomp
+puts response
