7997966615d2c807a3d52528a56ddca74cf4ed1c
agomezdemen <agomezdemen15@gmail.com>
2020-03-25 15:47:05 -0500

    Main menu added

--- bin/run.rb
+++ bin/run.rb
@@ -6,7 +6,23 @@ uri = URI.parse("https://api.seatgeek.com/2/events?client_id=MjExNDI3ODR8MTU4NTE
 response = Net::HTTP.get_response(uri)
 random_var_name = JSON.parse(response.body)
 
-(system "clear") || system("cls")
+def clear
+    (system "clear") || system("cls")
+end
+
+#user log in
+clear 
 puts "Welcome to project_name\n1. New User\n2. Login\n\n>"
 response = gets.chomp
-puts response
+
+#main menu
+clear
+puts "What would you like to do?\n"
+puts "1. Search events by event title"
+puts "2. Search events by performer"
+puts "3. Search events by venue"
+puts "4. TBD"
+puts "5. Quit\n"
+
+input = gets.chomp
+    
