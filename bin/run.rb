d0705e95ebd6a56bcb315868907f1b009a95e380
Jeff <jeff.fisk512@gmail.com>
2020-03-25 14:52:22 -0500

    API and Bundler set up

--- bin/run.rb
+++ bin/run.rb
@@ -2,4 +2,7 @@ require_relative '../config/environment'
 
 
 
-puts "HELLO WORLD"
+uri = URI.parse("https://api.seatgeek.com/2/events?client_id=MjExNDI3ODR8MTU4NTE2MzM2OS43")
+response = Net::HTTP.get_response(uri)
+random_var_name = JSON.parse(response.body)
+binding.pry
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
 require 'bundler'
 Bundler.require
 require_all 'app/models'
+require 'open-uri'
+require 'net/http'
+require 'json'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
